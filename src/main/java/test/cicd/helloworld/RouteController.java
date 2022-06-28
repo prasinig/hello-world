d1e8d7f4f34ce82da1fc071e6bfdeaf964013f16
P Singh <74644p@gmail.com>
2022-06-28 15:45:52 -0400

    T2 fake /health end point added

--- src/main/java/test/cicd/helloworld/RouteController.java
+++ src/main/java/test/cicd/helloworld/RouteController.java
@@ -2,6 +2,7 @@ package test.cicd.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import test.cicd.helloworld.entity.Health;
 
 @RestController
 public class RouteController {
@@ -9,5 +10,10 @@ public class RouteController {
     @GetMapping("/")
     public String index() {
         return "Hello World (V2)";
+    }
+
+    @GetMapping("health")
+    public Health health() {
+        return  new Health("OK", "This is a fake Health end point");
     }
 }
