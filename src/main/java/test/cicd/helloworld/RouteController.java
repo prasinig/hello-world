5e5b0378b0599660fe84d2d0c800f449b00d54c0
P Singh <74644p@gmail.com>
2022-06-28 15:32:42 -0400

    hello-world working in /

---
+++ src/main/java/test/cicd/helloworld/RouteController.java
@@ -0,0 +1,13 @@
+package test.cicd.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RouteController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+}
